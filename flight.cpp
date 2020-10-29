bac68089a67a5b05aa304b4bee180ab041ab815c
Reyna-5970 <55515949+Reyna-5970@users.noreply.github.com>
2020-10-28 18:32:24 -0700

    Starting planning process

--- flight.cpp
+++ flight.cpp
@@ -1,1 +1,9 @@
-// flight path executable file
+/* OBJECTIVE: generate trajectory, create PID controller to attain desired path 
+
+2D kinematics
+Manipulating forces --> changes acceleration (producing thrust)
+
+Control algorithm
+Linear algebra
+
+*/
