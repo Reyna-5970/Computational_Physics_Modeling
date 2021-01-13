5cd4ea2c0c77421f7fd495f3e1eff4442ea02929
Reyna-5970 <reyna.j.ayala@gmail.com>
2021-01-12 22:16:37 -0800

    Still not understanding non-static member functions

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 // standard libraries
 #include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 #include <math.h>
 
 // custom header files
 #include "flight_visuals.h"
 
-// instantiate
-#define flight_visuals Flight_Visuals;
-
 int main(void) {
-	Flight_Visuals::run;
+
+	// instantiate classes
+	Flight_Visuals flight_visuals;
+
+	flight_visuals.run;
 }
